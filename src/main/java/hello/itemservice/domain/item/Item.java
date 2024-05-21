437f4179266ee6a690665affea8081bf33cadf9e
idohyeon <ehgus5026@naver.com>
2024-05-22 02:58:57 +0900

    -Item domain(Validation)

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,30 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "가격 * 수량의 값이 10000원 이상이 되어야 합니다.") 범용성이 좋지 않아서 권장x(제약 조건)
 public class Item {
 
+//    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
     private Long id;
+
+    // hibernate validator가 제공하는 기본 오류 메시지를 사용하지 않고 이렇게 직접 설정 가능
+//    @NotBlank(message = "공백 불가능", groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
